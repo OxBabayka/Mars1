d5d16942c1a555a8018c1d30e3b8838c66576f01
OxBabayka <132775733+OxBabayka@users.noreply.github.com>
2025-05-04 14:15:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const tg = window.Telegram.WebApp;
+tg.ready();
+
+const user = tg.initDataUnsafe.user;
+const username = user ? user.username : 'колонист';
+
+document.querySelector('h1').innerText = `Добро пожаловать на Марс, ${username}!`;
+
 let resources = 0;
 
 function buildBuilding() {
