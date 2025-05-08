e122200926a024daee2a58fa32fafd38bf7367ac
OxBabayka <132775733+OxBabayka@users.noreply.github.com>
2025-05-08 12:01:36 +0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Million on Europa is starting...');
-    const game = new Game();
-    game.start();
-    setupExploreButton(game);
-});
-
